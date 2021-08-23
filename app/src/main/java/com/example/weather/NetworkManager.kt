9d64ffea3550c2bad3080b9c666882f803d68b7e
ivashkov_d <100100101104@mail.ru>
2021-08-23 14:54:39 +0300

    fix RetrofitClient.getClient, проверка ифа наоборот

--- app/src/main/java/com/example/weather/NetworkManager.kt
+++ app/src/main/java/com/example/weather/NetworkManager.kt
@@ -35,14 +35,12 @@ class NetworkManager @Inject constructor(/*private val connectivityManager: Conn
 
         fun getClient(baseUrl: String): Retrofit =
             retrofit?.let {
-                Retrofit.Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-            } ?: kotlin.run {
                 retrofit as Retrofit
-            }
-        }
+            } ?: Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
 
     interface RetrofitServicesByCityId {
         @GET("hourly?appid=b1b15e88fa797225412429c1c50c122a1")
